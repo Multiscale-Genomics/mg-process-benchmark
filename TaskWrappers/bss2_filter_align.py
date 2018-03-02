d737a610004dbe64c8c25fafb5d05e0b89c450a3
Mark McDowall <mark.mcdowall@gmail.com>
2018-03-02 09:09:31 +0000

    Initial commit of WGBS split, align, filter and merging

---
+++ TaskWrappers/bss2_filter_align.py
@@ -0,0 +1,137 @@
+"""
+.. See the NOTICE file distributed with this work for additional information
+   regarding copyright ownership.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+from __future__ import print_function
+
+# import logging
+import luigi
+
+from luigi.contrib.lsf import LSFJobTask
+
+from tool.bs_seeker_aligner import bssAlignerTool
+from tool.bs_seeker_filter import filterReadsTool
+
+# logger = logging.getLogger('luigi-interface')
+
+class TimeTaskBSSeekerFilterAlign(object):  # pylint: disable=too-few-public-methods
+    """
+    Timer object
+    """
+
+    @luigi.Task.event_handler(luigi.Event.PROCESSING_TIME)
+    def print_execution_time(self, processing_time):  # pylint: disable=no-self-use
+        """
+        Print the length of time the task ran for (seconds)
+        """
+        print('### PROCESSING TIME - BS Seeker 2 Filter and Align ###: ' + str(processing_time))
+
+class ProcessBSSeekerFilterAlignSingle(LSFJobTask, TimeTaskBSSeekerFilterAlign):
+
+    genome_fa = luigi.Parameter()
+    genome_idx = luigi.Parameter()
+    fastq_file = luigi.Parameter()
+    output_bam = luigi.Parameter()
+
+    def output(self):
+        """
+        Returns
+        -------
+        output : luigi.LocalTarget()
+            Location of the aligned reads in bam format
+        """
+        return luigi.LocalTarget(self.output_bam)
+
+    def work(self):
+        """
+        Worker function for aligning single ended FASTQ reads using Bowtie2
+
+        Parameters
+        ----------
+        genome_fa : str
+            Location of the FASTA file of the genome to align the reads to
+        genome_idx : str
+            Location of the index files in .tar.gz file prepared by the BWA
+            indexer
+        fastq_file : str
+            Location of the FASTQ file
+        output_bam : str
+            Location of the aligned reads in bam format
+        """
+
+        frt = filterReadsTool(self.configuration)
+        fastq1f, filter1_meta = frt.run(
+            {"fastq": input_files["fastq1"]},
+            {"fastq": metadata["fastq1"]},
+            {"fastq_filtered": output_files["fastq1_filtered"]}
+        )
+
+        bss_aligner = bssAlignerTool(self.configuration)
+        aligner_input_files = {
+            "genome" : input_files["genome"],
+            "fastq1" : fastq1f["fastq_filtered"],
+            "fastq2" : fastq2f["fastq_filtered"]
+        }
+
+        bam_handle = bamUtilsTask()
+        bam_handle.bam_sort(self.output_bam)
+
+class ProcessBSSeekerFilterAlignPaired(LSFJobTask, TimeTaskBSSeekerFilterAlign):
+
+    genome_fa = luigi.Parameter()
+    genome_idx = luigi.Parameter()
+    fastq_file_1 = luigi.Parameter()
+    fastq_file_2 = luigi.Parameter()
+    output_bam = luigi.Parameter()
+
+    def output(self):
+        """
+        Returns
+        -------
+        output : luigi.LocalTarget()
+            Location of the aligned reads in bam format
+        """
+        return luigi.LocalTarget(self.output_bam)
+
+    def work(self):
+        """
+        Worker function for aligning single ended FASTQ reads using Bowtie2
+
+        Parameters
+        ----------
+        genome_fa : str
+            Location of the FASTA file of the genome to align the reads to
+        genome_idx : str
+            Location of the index files in .tar.gz file prepared by the BWA
+            indexer
+        fastq_file : str
+            Location of the FASTQ file
+        output_bam : str
+            Location of the aligned reads in bam format
+        """
+
+        bowtie2_handle = bowtie2AlignerTool({"no-untar" : True})
+        bowtie2_handle.bowtie2_aligner_paired(
+            self.genome_fa,
+            self.fastq_file_1,
+            self.fastq_file_2,
+            self.output_bam,
+            self.genome_idx,
+            {}
+        )
+
+        bam_handle = bamUtilsTask()
+        bam_handle.bam_sort(self.output_bam)
